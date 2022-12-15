a222babbf3938b2a28ad05bc8680306f009d1de8
innocentcharlesudo@gmail.com <innocentcharlesudo@gmail.com@gmail.comgit config --global user.name Innocentsaxlsexitqgit config --global user.email innocentcharlesudo@gmail.com@gmail.com>
2022-12-15 14:14:36 -0800

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,11 +4,13 @@
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
-	i = 98 % 2;
+
+	i = 98
 	positive_or_negative(i);
 
 	return (0);
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -5,6 +5,7 @@
  * Description: print value of n status; zero, positive or negative
  * Return: Always 0 (Success)
  */
+
 void positive_or_negative(int i)
 {
 	if (i == 0)
