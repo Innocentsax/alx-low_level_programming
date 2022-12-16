b46ece4855ba9ab8cf50bc0efc42a1fa9d96e5c5
innocentcharlesudo@gmail.com <innocentcharlesudo@gmail.com@gmail.comgit config --global user.name Innocentsaxlsexitqgit config --global user.email innocentcharlesudo@gmail.com@gmail.com>
2022-12-15 18:59:10 -0800

    more print

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,7 @@ void more_numbers(void)
 		{
 			if (j >= 10)
 				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');	if (j >= 10)
-				
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
-	}	
-}
+	}
