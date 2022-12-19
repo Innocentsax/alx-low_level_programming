31a8244cdeb8d5342e80ee5a84bee566edb0bc09
innocentcharlesudo@gmail.com <innocentcharlesudo@gmail.com@gmail.comgit config --global user.name Innocentsaxlsexitqgit config --global user.email innocentcharlesudo@gmail.com@gmail.com>
2022-12-19 00:45:17 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+/**
+ * _strcpy - copies a string from one pointer to another
+ * including the terminating null byte (\0)
+ *
+ * @src: source of string parameter input
+ * @dest: destination of string
+ *
+ * Return: pointer to dest input parameter
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a, b = 0;
+
+	for (a = 0; src[a] != '\0'; ++a)
+	{
+		dest[b] = src[a];
+		++b;
+	}
+	dest[b] = '\0';
+
+	return (dest);
+}
