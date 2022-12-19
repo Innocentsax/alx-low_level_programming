d2c7010d719f67b4372f73f35b6750ae222ef725
innocentcharlesudo@gmail.com <innocentcharlesudo@gmail.com@gmail.comgit config --global user.name Innocentsaxlsexitqgit config --global user.email innocentcharlesudo@gmail.com@gmail.com>
2022-12-18 23:43:44 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+/**
+ * swap_int - swaps the values of two integers
+ * using two input parameters
+ *
+ * @a: input parameter 1
+ * @b: input parameter 2
+ *
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
