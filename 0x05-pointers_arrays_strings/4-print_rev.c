421a879a2aa21acb263d0c179cf424cda885a1e0
innocentcharlesudo@gmail.com <innocentcharlesudo@gmail.com@gmail.comgit config --global user.name Innocentsaxlsexitqgit config --global user.email innocentcharlesudo@gmail.com@gmail.com>
2022-12-18 23:57:32 -0800

    print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int index;
+
+	/*finds the length of string without null character*/
+	for (index = 0; s[index] != '\0'; ++index)
+		;
+
+	/*print char from the last index as you decrement*/
+	for (--index; index >= 0; --index)
+		_putchar(s[index]);
+	_putchar('\n');
+}
