4122b1fd65ea4fcb80b9d227b9bd1a25c563edf7
innocentcharlesudo@gmail.com <innocentcharlesudo@gmail.com@gmail.comgit config --global user.name Innocentsaxlsexitqgit config --global user.email innocentcharlesudo@gmail.com@gmail.com>
2022-12-22 00:07:17 -0800

    Pointers arrays string

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
