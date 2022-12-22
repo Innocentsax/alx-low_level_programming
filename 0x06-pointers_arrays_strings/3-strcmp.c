6fb8922dec424a4668cc973c2805cfc590b06d9c
innocentcharlesudo@gmail.com <innocentcharlesudo@gmail.com@gmail.comgit config --global user.name Innocentsaxlsexitqgit config --global user.email innocentcharlesudo@gmail.com@gmail.com>
2022-12-21 20:48:11 -0800

    strcmp.c & strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,12 +5,12 @@
  * @s2: against this other string 2
  *
  * Return: 0 if s1 and s2 are equal
- * 	negative integer if the stopping character
- * 		in @s1 was less than the stopping
- * 		character in @s2
- * 	positive integer if the stopping character
- * 		in @s1 was greater than the stopping
- * 		character in @s2
+ *	negative integer if the stopping character
+ *		in @s1 was less than the stopping
+ *		character in @s2
+ *	positive integer if the stopping character
+ *		in @s1 was greater than the stopping
+ *		character in @s2
  */
 int _strcmp(char *s1, char *s2)
 {
