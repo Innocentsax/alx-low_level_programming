20645911171410aed71cce800c7ad3e02aaaf357
innocentcharlesudo@gmail.com <innocentcharlesudo@gmail.com@gmail.comgit config --global user.name Innocentsaxlsexitqgit config --global user.email innocentcharlesudo@gmail.com@gmail.com>
2023-01-02 04:43:57 -0800

    Pointer, strings and Arrays

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
 int _putchar(char c);
 
 char *_memset(char *s, char b, unsigned int n);
@@ -17,3 +21,6 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 
 void set_string(char **s, char *to);
+
+int _putchar(char c);
+#endif
