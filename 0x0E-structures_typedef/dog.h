1bf6716bf0bead668485e6444a6a042da68d3944
Innocentsax <innocentcharlesudo@gmail.com>
2023-01-16 21:04:10 +0100

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,8 +17,8 @@ struct dog
 };
 
 /**
- *  * dog_t -Typedef for struct dog
- *  */
+ * dog_t -Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
