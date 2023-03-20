4642e36c1d9ef5de5a3105a3e10875d7acd2dce8
Innocentsax <innocentcharlesudo@gmail.com>
2023-03-20 23:55:21 +0100

    Dynamic Libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+		return (a + b);
+}
+
+int sub(int a, int b)
+{
+		return (a - b);
+}
+int mul(int a, int b)
+{
+		return (a * b);
+}
+
+int div(int a, int b)
+{
+		return (a / b);
+}
+
+int mod(int a, int b)
+{
+		return (a % b);
+}
