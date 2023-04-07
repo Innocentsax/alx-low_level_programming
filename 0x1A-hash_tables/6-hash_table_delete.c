6703997e2a0f0feb9faa0c04fcfa36b331bfbea4
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-04-07 08:10:29 +0100

    Hash Table in Python

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,30 +1,29 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_delete - Deletes a hash table.
- * @ht: A pointer to a hash table.
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *head = ht;
-	hash_node_t *node, *tmp;
+	hash_node_t *node, *temp;
 	unsigned long int mx;
+
+	if (!ht)
+		return;
 
 	for (mx = 0; mx < ht->size; mx++)
 	{
-		if (ht->array[mx] != NULL)
+		node = ht->array[mx];
+		while (node)
 		{
-			node = ht->array[mx];
-			while (node != NULL)
-			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
-			}
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = temp;
 		}
 	}
-	free(head->array);
-	free(head);
+
+	free(ht->array);
+	free(ht);
 }
